d56aae2ce7c4d5bb31a839140a7db8ff20043bb3
koen-db <koen.de.bruyne@howest.be>
2022-05-09 09:40:05 +0200

    Add hardcoded api request and response

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -9,7 +11,16 @@ def index():
 @app.route("/search")
 def search():
     searchterm = request.args.get("searchterm")
-    return render_template("search.html",searchterm=searchterm)
+    #API code here
+    #URL: https://test.wikipedia.org/w/api.php?format=json&action=query&list=search&srsearch=Japan
+
+    url = "https://test.wikipedia.org/w/api.php?format=json&action=query&list=search&srsearch=Japan"
+    response = requests.get(url)
+
+    article = json.dumps(response.json(), indent=4)
+    print(article)
+    
+    return render_template("search.html",searchterm=searchterm,article=article)
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080)
