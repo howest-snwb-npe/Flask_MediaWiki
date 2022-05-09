20aff1c906f5b749d7a36019259bf472202e9e1d
koen-db <koen.de.bruyne@howest.be>
2022-05-09 09:17:05 +0200

    Add form and output

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    name = "Flask"
-    return render_template("index.html",name=name)
+    return render_template("index.html")
+
+@app.route("/search")
+def search():
+    searchterm = request.args.get("searchterm")
+    return render_template("search.html",searchterm=searchterm)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8080)
